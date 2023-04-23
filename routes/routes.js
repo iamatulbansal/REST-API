d2b88d360511a269fc7aa0b959ff696296eb518d
ATUL BANSAL <86524139+iamatulbansal@users.noreply.github.com>
2023-04-24 03:48:53 +0530

    remove verification for all api

--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,6 @@ const PATH = require('../constants/url')
 module.exports = (app) => {
     app.post(PATH.REGISTER, controller.register);
     app.post(PATH.LOGIN, controller.login);
-    app.get(PATH.ALL_USERS, verify, controller.users);
+    app.get(PATH.ALL_USERS, controller.users);
     app.get(PATH.PROFILE, verify, controller.profile);
 };
